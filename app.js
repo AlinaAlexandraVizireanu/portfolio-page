8ef58e02bf772d94052328d8a84fd77603a05faf
Alina Vizireanu <vizireanualina15@yahoo.com>
2024-08-04 17:51:24 +0300

    implement navbar functionality fully

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const menuBtn = document.querySelector(".menuBtn");
 const menuContent = document.querySelector(".menu");
 
-menuBtn.addEventListener("click", () => {
-  menuContent.classList.toggle("hidden");
+function toggleClasses() {
+  const classesToToggle = ["min-h-[30vh]", "opacity-100", "visible"];
 
-  if (!menuContent.classList.contains("hidden")) {
+  classesToToggle.forEach((className) => {
+    menuContent.classList.toggle(className);
+  });
+}
+
+menuBtn.addEventListener("click", () => {
+  const classesToToggle = ["min-h-[30vh]", "opacity-100", "visible"];
+
+  classesToToggle.forEach((className) => {
+    menuContent.classList.toggle(className);
+  });
+
+  if (menuContent.classList.contains("visible")) {
+    menuContent.classList.remove("invisible", "h-0", "opacity-0");
+  } else {
+    menuContent.classList.add("invisible", "h-0", "opacity-0");
+  }
+
+  if (menuContent.classList.contains("visible")) {
     menuBtn.src = "./assets/images/icon-menu-close.svg";
+    menuBtn.alt = "close";
   } else {
     menuBtn.src = "./assets/images/icon-menu.svg";
+    menuBtn.alt = "open";
   }
 });
