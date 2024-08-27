ad9e766dad3efb8f3c6bd208dd0891d711dd119a
Alina Vizireanu <vizireanualina15@yahoo.com>
2024-08-27 22:57:43 +0300

    implement animation for the background

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const menuBtn = document.querySelector(".menuBtn");
 const menuContent = document.querySelector(".menu");
+const interBubble = document.querySelector(".interactive");
+
+let curX = 0;
+let curY = 0;
+let tgX = 0;
+let tgY = 0;
 
 function toggleClasses() {
   const classesToToggle = ["min-h-[30vh]", "opacity-100", "visible"];
@@ -8,6 +14,25 @@ function toggleClasses() {
     menuContent.classList.toggle(className);
   });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  function move() {
+    curX += (tgX - curX) / 20;
+    curY += (tgY - curY) / 20;
+    interBubble.style.transform = `translate(${Math.round(
+      curX
+    )}px, ${Math.round(curY)}px)`;
+    requestAnimationFrame(() => {
+      move();
+    });
+  }
+  window.addEventListener("mousemove", (e) => {
+    tgX = e.clientX;
+    tgY = e.clientY;
+  });
+
+  move();
+});
 
 menuBtn.addEventListener("click", () => {
   const classesToToggle = ["min-h-[30vh]", "opacity-100", "visible"];
